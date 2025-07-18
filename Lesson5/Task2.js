04855e6d7408b61d9d15df205265ac36638883a0
Den Yeromenko <den.eremenko.1476@gmail.com>
2025-07-18 18:15:29 +0300

    Lesson5 Tasks

---
+++ Lesson5/Task2.js
@@ -0,0 +1,19 @@
+
+let averageGrade = 98
+switch (true) {
+    case averageGrade < 60:
+        console.log("Незадовільно");
+        break;
+    case averageGrade <= 70:
+        console.log("Задовільно");
+        break;
+    case averageGrade <= 80:
+        console.log("Добре");
+        break; case averageGrade <= 90:
+        console.log("Дуже Добре");
+        break;
+    case averageGrade <= 100:
+        console.log("Відмінно");
+        break;
+}
+
---
+++ Lesson5/Task3.js
@@ -0,0 +1,7 @@
+let i = 1;
+let number = 3
+
+while (i <= 10) {
+    console.log(`${number} x ${i} = ${number * i}`);
+    i++;
+}
